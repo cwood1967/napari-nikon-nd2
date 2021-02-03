7422e4fbd418170e71184e8b69824e5a6d021070
Chris Wood <cjw@stowers.org>
2021-02-03 15:32:50 -0600

    added version to setup

--- setup.py
+++ setup.py
@@ -26,9 +26,10 @@ use_scm = {"write_to": "napari_nikon_nd2/_version.py"}
 
 setup(
     name='napari-nikon-nd2',
+    version="0.1.1"
     author='Chris Wood',
     author_email='cwood1967@gmail.com',
-    license='Apache Software License 2.0',
+    license='Apache License 2.0',
     url='https://github.com/cwood1967/napari-nikon-nd2',
     description='Opens Nikon ND2 files into napari.',
     long_description=read('README.md'),
@@ -49,7 +50,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Operating System :: OS Independent',
-        'License :: OSI Approved :: Apache Software License',
+        'License :: OSI Approved :: Apache License',
     ],
     entry_points={
         'napari.plugin': [
